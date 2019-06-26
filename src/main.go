74d00fac5a217f50ca38a47f44bc92c48ad04f93
Roy Lin <rlin2k1@gmail.com>
2019-06-26 12:01:35 -0700

    Adds basic webpage design for file uploads.

---
+++ src/main.go
@@ -0,0 +1,135 @@
+/* main.go
+Web Application to Provide an easy and simple way for Poshmark Marketing Team to
+upload images to Amazon S3 for marketing campaigns.
+
+Author(s):
+	Roy Lin
+
+Date Created:
+	June 25th, 2019
+*/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html")) //MUST Panics if ParseFiles returns ERROR
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")     //MUST Will Panic If FAILS
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	//t, err := template.ParseFiles(tmpl + ".html")
+	//if err != nil {
+	//	http.Error(w, err.Error(), http.StatusInternalServerError)
+	//	return
+	//}
+	//err = t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	// Here we will extract the page title from the Request, and call the provided handler 'fn'
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	// title, err := getTitle(w, r)
+	// if err != nil {
+	// 	return
+	// }
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	// title, err := getTitle(w, r)
+	// if err != nil {
+	// 	return
+	// }
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	// title, err := getTitle(w, r)
+	// if err != nil {
+	// 	return
+	// }
+	body := r.FormValue("body")                  //Most Important!
+	p := &Page{Title: title, Body: []byte(body)} //Convert body{String} to {byte}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
